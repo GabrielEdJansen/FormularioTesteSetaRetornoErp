ae109764a69e84dba36d62d40d81ddddde42b110
GabrielEdJansen <93282987+GabrielEdJansen@users.noreply.github.com>
2022-01-25 14:51:46 -0300

    Update bpm.js

--- bpm.js
+++ bpm.js
@@ -1,3 +1,117 @@
+function exibiParcelasComValorAcimaDeZero() {
+
+    if (document.getElementById("VlrParDois").value = 0) {
+        let eBol2 = document.getElementById('parcela2');
+
+        eBol2.style.display = 'none';
+
+        let eBol = document.getElementById('parcela2Bol');
+
+        eBol.style.display = 'none';
+    }  
+
+    if (document.getElementById("VlrParTres").value = 0) {
+        let eBol3 = document.getElementById('parcela3');
+
+        eBol3.style.display = 'none';
+
+        let eBol = document.getElementById('parcela3Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParQuatro").value = 0) {
+        let eBol4 = document.getElementById('parcela4');
+
+        eBol4.style.display = 'none';
+
+        let eBol = document.getElementById('parcela4Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParCinco").value = 0) {
+        let eBol5 = document.getElementById('parcela5');
+
+        eBol5.style.display = 'none';
+
+        let eBol = document.getElementById('parcela5Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParSeis").value = 0) {
+        let eBol6 = document.getElementById('parcela6');
+
+        eBol6.style.display = 'none';
+
+        let eBol = document.getElementById('parcela6Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParSete").value = 0) {
+        let eBol7 = document.getElementById('parcela7');
+
+        eBol7.style.display = 'none';
+
+        let eBol = document.getElementById('parcela7Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParOito").value = 0) {
+        let eBol8 = document.getElementById('parcela8');
+
+        eBol8.style.display = 'none';
+
+        let eBol = document.getElementById('parcela8Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParNove").value = 0) {
+        let eBol9 = document.getElementById('parcela9');
+
+        eBol9.style.display = 'none';
+
+        let eBol = document.getElementById('parcela9Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParDez").value = 0) {
+        let eBol10 = document.getElementById('parcela10');
+
+        eBol10.style.display = 'none';
+
+        let eBol = document.getElementById('parcela10Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParOnze").value = 0) {
+        let eBol11 = document.getElementById('parcela11');
+
+        eBol11.style.display = 'none';
+
+        let eBol = document.getElementById('parcela11Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+    if (document.getElementById("VlrParDoze").value = 0) {
+        let eBol12 = document.getElementById('parcela12');
+
+        eBol12.style.display = 'none';
+
+        let eBol = document.getElementById('parcela12Bol');
+
+        eBol.style.display = 'none';
+    } 
+
+}
+
 function selecionaTipoFormulario() {
     if (document.getElementById("selectTipFor").value == 'Adiantamento') {
 
@@ -448,6 +562,7 @@ function _init(data, info) {
             selecionaTipoFormulario()
             ExibeBoleto()
             ExibirFormaPagamento()
+            exibiParcelasComValorAcimaDeZero()
         }
     });
 }
